44149a7421a5846fba730481cd3c429e8c09ffe6
Shota Iguchi <shota-iguchi@cookpad.com>
2020-06-07 22:03:50 +0900

    split bench

---
+++ bench.sh
@@ -0,0 +1,2 @@
+set -ex
+./bin/benchmarker -target-url http://ec2-54-199-15-35.ap-northeast-1.compute.amazonaws.com/
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 set -ex
 
 cd ~/isucon9-qualify/webapp/go/ && rm -rf isucari && make
-cd ~/isucon9-qualify/ && sudo systemctl restart isucari && ./bin/benchmarker -target-url http://ec2-54-199-15-35.ap-northeast-1.compute.amazonaws.com/
+cd ~/isucon9-qualify/ && sudo systemctl restart isucari
